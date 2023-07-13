c2d4cac1dde39fc09ac21bf938f862021ff3780b
Yusuph Omari <57600316+Yusuf-96@users.noreply.github.com>
2023-07-13 15:42:04 +0300

    sidebar

--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -7,3 +7,132 @@ const UserTable = () => {
 }
 
 export default UserTable
+
+
+import React from "react";
+import { ReactComponent as DashboardIcon } from "../../assets/dashboardicon.svg";
+import { ReactComponent as ProductIcon } from "../../assets/producticon.svg";
+import { ReactComponent as ShopIcon } from "../../assets/shopicon.svg";
+import { ReactComponent as PointSaleIcon } from "../../assets/pointsaleicon.svg";
+
+export const SidebarData = [
+  {
+    id: 1,
+    title: "Dashboard",
+    path: "/",
+    icon: <DashboardIcon />,
+  },
+
+  {
+    id: 2,
+    title: "shop",
+    path: "/shop",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+        />
+      </svg>
+    ),
+  },
+  {
+    id: 3,
+    title: "Category",
+    path: "/orders",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+        />
+      </svg>
+    ),
+  },
+  {
+    id: 4,
+    title: "Product",
+    path: "/product",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+        />
+      </svg>
+    ),
+  },
+  {
+    id: 5,
+    title: "Point of Sale",
+    path: "/salepoint",
+    icon: <PointSaleIcon />,
+  },
+  {
+    id: 6,
+    title: "Report",
+    path: "/report",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+        />
+      </svg>
+    ),
+  },
+
+  {
+    id: 7,
+    title: "User",
+    path: "/user",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+        />
+      </svg>
+    ),
+  },
+];
